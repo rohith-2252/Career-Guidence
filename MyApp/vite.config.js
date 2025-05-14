5aeb63aea2954ebff60f2f5fc2943f41a0cc5a84
rohith-2252 <rohith.r2252@gmail.com>
2025-05-14 13:09:15 +0530

    Update vite.config.js

--- MyApp/vite.config.js
+++ MyApp/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mdx from '@mdx-js/rollup'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:"/Career-Guidence"
+  plugins: [
+    { enforce: 'pre', ...mdx() },
+    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
+  ],
 })
